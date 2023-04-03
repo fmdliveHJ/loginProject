8e389b067aa0265991d70130b603f8c1d98dcc6c
hyeonjin <gdalove@naver.com>
2023-04-03 22:35:44 +0900

    action 수정

--- src/actions/user_action.js
+++ src/actions/user_action.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const LOGIN_USER = "login_user";
-export const REGISTER_USER = "register_user";
+export const LOGIN_USER = 'login_user';
+export const REGISTER_USER = 'register_user';
 
-//email, password 받은 파라미터를 dataTsubmit에 넣어줌
-export function loginUser(dataTsubmit) {
-  const request = axios.post(url, dataTsubmit).then((res) => res.data);
+//email, password 받은 파라미터를 dataSubmit에 넣어줌
+export function loginUser(dataSubmit) {
+  const request = axios.post(url, dataSubmit).then((res) => res.data);
   return {
-    type: "LOGIN_USER",
+    type: 'LOGIN_USER',
     payload: request,
   };
 }
 
-export function registerUser(dataTsubmit) {
-  const request = axios.post(url, dataTsubmit).then((res) => {
-    res.data;
+export function registerUser(dataSubmit) {
+  const request = axios.post(url, dataSubmit).then((res) => {
+    return res.data;
   });
   return {
-    type: "REGISTER_USER",
+    type: 'REGISTER_USER',
     payload: request,
   };
 }
